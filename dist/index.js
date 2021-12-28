7e67743cd8f87cab40353eaf718f469ba9605d7f
szenius <ting.szeying@gmail.com>
2021-12-28 17:20:22 +0000

    build: update dist files

--- dist/index.js
+++ dist/index.js
@@ -17859,23 +17859,23 @@ const updateDailySummaryTable = async (
           },
         ],
       },
-      Sprint: {
-        type: "multi_select",
-        multi_select:{
-          options:[
-            {
-              name: `2222`,
-              color:"blue"
-            }
-          ]
-        }
-      },
-      Points: {
-        number: pointsLeft,
-      },
-      Date: {
-        date: { start: today, end: null },
-      },
+//       Sprint: {
+//         type: "multi_select",
+//         multi_select:{
+//           options:[
+//             {
+//               name: `2222`,
+//               color:"blue"
+//             }
+//           ]
+//         }
+//       },
+//       Points: {
+//         number: pointsLeft,
+//       },
+//       Date: {
+//         date: { start: today, end: null },
+//       },
     },
   });
   
