a1b93cc26c07b5b2919b599300b80a1a9968deae
ezlukeyuan <96225530+ezlukeyuan@users.noreply.github.com>
2021-12-30 02:39:19 +0800

    Update index.js
    title

--- index.js
+++ index.js
@@ -381,15 +381,13 @@ const generateChart = (data, idealBurndown, labels , demo, goal) => {
         ],
       },
       options: {
-        plugins: {
-            title: {
-                display: true,
-                text: '燃盡圖',
-            },
-            subtitle: {
-                display: true,
-                text: goal.unshift('Demo:'+ demo),
-            }
+        title: {
+            display: true,
+            text: '燃盡圖',
+        },
+        subtitle: {
+            display: true,
+            text: goal.unshift('Demo:'+ demo),
         },
         legend: { display: false },
         scales: {
