99c35be16a41bcb2dc2935a71b9fe5ae1a574f7a
ezlukeyuan <96225530+ezlukeyuan@users.noreply.github.com>
2022-01-03 17:34:21 +0800

    Update index.js
      log.info('WebClient:',WebClient);

--- index.js
+++ index.js
@@ -434,6 +434,7 @@ const writeChartToFile = async (chart, dir, filenamePrefix) => {
 const sendSlackMessage = async () => {
   const web = new WebClient(process.env.SLACK_TOKEN);
   log.info('WebClient:',WebClient);
+  log.info('SLACK_TOKEN:',process.env.SLACK_TOKEN);
   // The current date
   const currentTime = new Date().toTimeString();
   try {
