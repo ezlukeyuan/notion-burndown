a118683ab0ab236dc372700d3292d254e34e9208
szenius <ting.szeying@gmail.com>
2022-01-16 17:41:05 +0000

    build: update dist files

--- dist/index.js
+++ dist/index.js
@@ -26858,9 +26858,6 @@ const generateChart = (data, idealBurndown, labels, demo, goal, progressByDay) =
               ticks: {
                 beginAtZero: true,
                 max:1,
-                callback: function(value, index, values) {
-                  return value * 100 + '%';
-                }
               },
               id: 'y2',
             },
@@ -26893,8 +26890,8 @@ const sendSlackMessage = async (filename,demo,goal) => {
   try {
     // Use the `chat.postMessage` method to send a message from this app
     await web.chat.postMessage({
-//       channel: 'C01TM4WSVH6',
-      channel: 'C0234HEGCT0',
+      channel: 'C01TM4WSVH6',
+//       channel: 'C0234HEGCT0',
       text:'每日Sprint目標',
       blocks: message
     });
