93fc26276cc6d96fb75b6569dc016ce3eaa6fbc1
szenius <ting.szeying@gmail.com>
2022-01-16 17:19:04 +0000

    build: update dist files

--- dist/index.js
+++ dist/index.js
@@ -26796,14 +26796,14 @@ const generateChart = (data, idealBurndown, labels, demo, goal, progressByDay) =
             yAxisID: 'y1',
           },
           {
-            label: "Constant",
+            label: "Ideal",
             borderColor: "#cad0d6",
             backgroundColor: "rgba(54,+162,+235,+.5)",
             data: idealBurndown,
             yAxisID: 'y1',
           },
           {
-            label: "Constant",
+            label: "Progress %",
             number:[5, 15],
             data: progressByDay,
             fill: 'origin',
@@ -26820,7 +26820,12 @@ const generateChart = (data, idealBurndown, labels, demo, goal, progressByDay) =
 //             display: true,
 //             text: goal.unshift('燃盡圖','Demo:'+ demo),
 //         },
-        legend: { display: false },
+        legend: {
+          display: true,
+          labels: {
+              fontColor: 'rgb(255, 99, 132)'
+          },
+        },
         scales: {
           xAxes: [
             {
@@ -26846,6 +26851,14 @@ const generateChart = (data, idealBurndown, labels, demo, goal, progressByDay) =
             {
               type: 'linear',
               position: 'right',
+              scaleLabel: {
+                display: true,
+                labelString: "Points Left",
+              },
+              ticks: {
+                beginAtZero: true,
+                max:1,
+              },
               id: 'y2',
             },
           ],
